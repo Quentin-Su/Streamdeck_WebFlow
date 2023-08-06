7dc6b96d4e0d9b4e1884846278ae33651c74b088
Quentin-Su <Quentin.Sussetto@outlook.fr>
2023-08-06 23:21:32 +0200

    feat: Add functionality to open a new tab

---
+++ WebFlow/Models/Browser.cs
@@ -0,0 +1,16 @@
+﻿using Newtonsoft.Json;
+
+namespace WebFlow.Models
+{
+    public class Browser
+    {
+        [JsonProperty(PropertyName = "browserName")]
+        public string DisplayName { get; set; }
+
+        [JsonProperty(PropertyName = "browserPath")]
+        public string BrowserPath { get; set; }
+
+        [JsonProperty(PropertyName = "argument")]
+        public string Argument { get; set; }
+    }
+}
